aab7af9e511f7bf6c8798eb48d389b61226f20b7
ekelly <eric.k.125@gmail.com>
2015-03-10 15:43:38 -0400

    Fixed FrameImage bugs

--- src/javalib/worldimages/FrameImage.java
+++ src/javalib/worldimages/FrameImage.java
@@ -52,7 +52,7 @@ public class FrameImage extends RectangleImage {
      * Produce a <code>String</code> representation of this rectangle image
      */
     public String toString() {
-        return "new RectangleImage(this.pinhole = (" + this.pinhole.x + ", "
+        return "new FrameImage(this.pinhole = (" + this.pinhole.x + ", "
                 + this.pinhole.y + "), \nthis.color = " + this.color.toString()
                 + "\nthis.width = " + width + ", this.height = " + height
                 + ")\n";
@@ -68,22 +68,23 @@ public class FrameImage extends RectangleImage {
      */
     public String toIndentedString(String indent) {
         indent = indent + "  ";
-        return classNameString(indent, "RectangleImage")
+        return classNameString(indent, "FrameImage")
                 + pinholeString(indent, this.pinhole)
                 + colorString(indent, this.color) + "\n" + indent
                 + "this.width = " + width + ", this.height = " + height + ")\n";
     }
 
     /**
-     * Is this <code>RectangleImage</code> same as the given object?
+     * Is this <code>FrameeImage</code> same as the given object?
      */
     public boolean equals(Object o) {
-        if (o instanceof RectangleImage) {
-            RectangleImage that = (RectangleImage) o;
+        if (o instanceof FrameImage) {
+            FrameImage that = (FrameImage) o;
             return this.pinhole.x == that.pinhole.x
                     && this.pinhole.y == that.pinhole.y
                     && this.width == that.width && this.height == that.height
-                    && this.color.equals(that.color);
+                    && this.color.equals(that.color)
+                    && this.img.equals(that.img);
         } else
             return false;
     }
